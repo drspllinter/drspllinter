b010a223c4066083ed80dd2527d27a615b3aad3f
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-05 18:48:34 +0200

    Update ringl.c

--- ringl.c
+++ ringl.c
@@ -3,28 +3,29 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
+  // Initialize the MPI environment
   MPI_Init(NULL, NULL);
+  // Find out rank, size
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  int token;
+  if (world_rank==0) {
+    char token='L';
+  }
+   MPI_Send(&token, 1, MPI_BYTE, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
   if (world_rank != 0) {
-    MPI_Recv(&token, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD,
+    MPI_Recv(&token, 1, MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
     printf("Process %d received token %d from process %d\n", world_rank, token,
-           world_rank - 1);
-  } else {
-    token = -1;
-  }
-  MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0,
-           MPI_COMM_WORLD);
-  if (world_rank == 0) {
-    MPI_Recv(&token, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD,
+world_rank - 1);
+  } else{
+    MPI_Recv(&token, 1, MPI_BYTE, world_size-1, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
-    printf("Process %d received token %d from process %d\n", world_rank, token,
-           world_size - 1);
+    printf("Process %d received token %c from process %d\n", world_rank, tokenn,
+           0);     
   }
+
   MPI_Finalize();
 }

ed9df0f43d31950046acce9c44f326e54e7ccd2e
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-05 19:52:50 +0200

    Update ring.c

--- ring.c
+++ ring.c
@@ -13,29 +13,29 @@ int main(int argc, char** argv) {
   
   char token='L';
   char tokenn='R';
-  if(world_rank==0){
-   MPI_Send(&token, 1, MPI_BYTE, world_rank+1, 0, MPI_COMM_WORLD); 
+  if(world_rank == 0){
+   MPI_Send(&token, 1, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD); 
    MPI_Send(&tokenn, 2, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD); 
    MPI_Recv(&token, 1, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
    printf("Process %d received token %c from process %d\n", world_rank, token, world_size - 1);
-   MPI_Recv(&tokenn, 2, MPI_BYTE, world_rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   MPI_Recv(&tokenn, 2, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
    printf("Process %d received token %c from process %d\n", world_rank, tokenn, 1);     
   }
-  else if (world_rank==world_size-1){
+  else if (world_rank == world_size - 1){
    MPI_Send(&token, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD); 
-   MPI_Send(&tokenn, 2, MPI_BYTE, world_rank-1, 0, MPI_COMM_WORLD); 
-   MPI_Recv(&token, 1, MPI_BYTE, world_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   MPI_Send(&tokenn, 2, MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD); 
+   MPI_Recv(&token, 1, MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
    printf("Process %d received token %c from process %d\n", world_rank, token, world_rank - 1);
    MPI_Recv(&tokenn, 2, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
    printf("Process %d received token %c from process %d\n", world_rank, tokenn, 0);  
   }  
   else {
-   MPI_Send(&token, 1, MPI_BYTE, world_rank+1, 0, MPI_COMM_WORLD); 
+   MPI_Send(&token, 1, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD); 
    MPI_Send(&tokenn, 2, MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD);  
-   MPI_Recv(&token, 1, MPI_BYTE, world_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   MPI_Recv(&token, 1, MPI_BYTE, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
    printf("Process %d received token %c from process %d\n", world_rank, token, world_rank - 1);
-   MPI_Recv(&tokenn, 2, MPI_BYTE, world_rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-   printf("Process %d received token %c from process %d\n", world_rank, tokenn, world_rank+1);     
+   MPI_Recv(&tokenn, 2, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   printf("Process %d received token %c from process %d\n", world_rank, tokenn, world_rank + 1);     
   }  
   MPI_Finalize();
 }  

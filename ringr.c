7dbddb4e80c6bcc0e412f210d9fd0615b63bb303
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-05 18:59:00 +0200

    Update ringr.c

--- ringr.c
+++ ringr.c
@@ -19,9 +19,8 @@ int main(int argc, char** argv) {
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  
+  char tokenn='R';
   if (world_rank==0) {
-    char tokenn='R';
     MPI_Send(&tokenn, 2, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD); 
   }
   if (world_rank != world_size-1) {

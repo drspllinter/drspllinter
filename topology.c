c6db55c924693768d0d31a5a204a4b8887b124db
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-06 22:20:15 +0200

    Create topology.c

---
+++ topology.c
@@ -0,0 +1,12 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Find out rank, size
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+MPI_Comm_size(MPI_COMM_WORLD, &world_size);

5b881115cc037db72d4e9bc7468175f1c5d954a9
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-04-06 22:36:04 +0200

    Update topology.c

--- topology.c
+++ topology.c
@@ -1,12 +1,22 @@
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char** argv) {
-  // Initialize the MPI environment
-  MPI_Init(NULL, NULL);
-  // Find out rank, size
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  int world_size;
-MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+/*Run with 8 processes*/
+voidmain (int argc, char *argv[])
+{
+  int rank;
+  MPI_Comm vu;
+  int dim[2], period[2], reorder;
+  int coord[2], id;
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  dim[0]=4;
+  dim[1]=2;
+  period[0]=TRUE;
+  period[1]=FALSE;
+  reorder=TRUE;
+  MPI_Cart_create(MPI_COMM_WORLD,2,dim,period,reorder,&vu);
+  if(rank==5){
+   MPI_Cart_coords(vu,rank,2,coord);
+   printf("P:%d My coordinates are %d %d\n",rank,coord[0],coord[1]);
+  } 
+  MPI_Finalize();
+}

7c1a7262c641c0bb444bfdfb7d453c0dd6c2e8c4
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-05-17 18:30:07 +0200

    Update and rename Cholesky4x4.C to Cholesky4x4.c

--- Cholesky4x4.c
+++ Cholesky4x4.c
@@ -11,15 +11,20 @@ int main(int argc, char** argv) {
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   
-  char token='L';
-  char tokenn='R';
+  double M[4][4] = {
+	{9, 0, -27, 18},
+	{0, 9, -9, -27},
+	{-27, -9, 99, -27},
+	{18, -27, -27, 121}
+};
+  
   if(world_rank == 0){
-   MPI_Send(&token, 1, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD); 
-   MPI_Send(&tokenn, 2, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD); 
-   MPI_Recv(&token, 1, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-   printf("Process %d received token %c from process %d\n", world_rank, token, world_size - 1);
-   MPI_Recv(&tokenn, 2, MPI_BYTE, world_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-   printf("Process %d received token %c from process %d\n", world_rank, tokenn, 1);     
+    for (int i = 0; i <= 3; i++){
+		  for (int j = 0; j <= 3; j++){
+			  cout << M[i][j] << " ";
+		  }
+		  cout << endl;
+	  }     
   }
   else if (world_rank == world_size - 1){
    MPI_Send(&token, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD); 
